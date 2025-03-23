cbe7b743d425eef629352533a52a030035a9b40c
Unnat Malik <160745526+UnnatMalik@users.noreply.github.com>
2025-03-23 21:26:32 +0530

    Update settings.py

--- BankPortal/settings.py
+++ BankPortal/settings.py
@@ -99,6 +99,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Use your email provider's SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'parvinder221978@gmail.com'
-EMAIL_HOST_PASSWORD = 'dgya seak vret yltr'  # Use an App Password for security
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''  # Use an App Password for security
 
